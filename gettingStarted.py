617e505bf4d81b6fce4d22147c93a9c4e214ecfc
gfs7337 <gfs7337@nyu.edu>
2024-09-13 13:19:23 -0400

    Try 4
    Try 4

--- gettingStarted.py
+++ gettingStarted.py
@@ -6,9 +6,23 @@ def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "Yes"
+    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "1080827fabfcc7a9d3f5703566ebb186dc946c80eb1b90a1f3c39adab271716a"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = "5"
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = "3"
     else:
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
@@ -23,26 +37,5 @@ if __name__ == "__main__":
     debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
 
-#Questions:
-if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-    answer = "pcap"
-elif question =="Are encoding and encryption the same? - Yes/No":
-    answer = "No"
-elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-    answer = "Yes"
-elif question =="Is it possible to decode a message without a key? - Yes/No":
-    answer = "Yes"
-elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-    answer = "No"
-elif question ==  "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-    answer = "1080827fabfcc7a9d3f5703566ebb186dc946c80eb1b90a1f3c39adab271716a"
-elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-    answer = "No"
-elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-    answer = "7"
-elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-    answer = "3"
-else:
-    answer = "Getting Started Assignment 1 Completed"
 
 
