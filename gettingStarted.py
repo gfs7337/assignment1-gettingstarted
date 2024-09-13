92b2056b6e874f9885ab9d789242ddac3f6aba7d
gfs7337 <gfs7337@nyu.edu>
2024-09-13 13:13:31 -0400

    Answered Questions Try 2
    Answered Questions Try 2

--- gettingStarted.py
+++ gettingStarted.py
@@ -11,7 +11,7 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "Yes"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
@@ -19,7 +19,7 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "7"
+        answer = "5"
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = "3"
     else:
